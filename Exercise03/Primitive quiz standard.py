59bc02a0054590412819bd1f33dd23805027fed9
Farah-vx <ocyeanic.08@gmail.com>
2024-10-30 12:08:12 +0400

    Primitive quiz standard.py

---
+++ Exercise03/Primitive quiz standard.py
@@ -0,0 +1,8 @@
+#compose the capital of france
+capital = input("what is the capital of france?: ")
+#drafting the answer as correct
+if capital == "Paris":
+    print("the answer is correct")
+#drafting the answer as incorrect
+else:
+    print("the answer is incorrect")

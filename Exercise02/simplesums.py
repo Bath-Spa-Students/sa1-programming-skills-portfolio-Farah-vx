d38600e7569c4d4a3a742e5426806da8a9031b57
Farah-vx <ocyeanic.08@gmail.com>
2024-10-30 12:09:38 +0400

    simple sum.py
    simple sum

---
+++ Exercise02/simplesums.py
@@ -0,0 +1,8 @@
+#initialize a variable with an integer value of 8.
+Amount1 = 8 
+#initialize a variable with an integer value of 10. 
+Amount2 = 10
+#declare a third variable to hold the sum of the first two numbers.
+total = Amount1 + Amount2 
+#printing the value
+print("the total sum is : ",total)

a16497f4285c6c1fca75fd3054340fbb3d2ad86d
Farah-vx <ocyeanic.08@gmail.com>
2024-11-07 14:47:27 +0400

    Primitive quiz standard.py

---
+++ practise/practise.py
@@ -0,0 +1,16 @@
+#define the number of interations
+for i in range (10):
+#statement that gets repeated  
+     print ("Hello")
+ #creating nos in sequence
+for i in range (10):
+     print (i)
+     #using any variable instead of i
+for num in range (10):
+     print (num)
+
+#while loop
+seat = 100 #initial no. of seats 
+while seat > 0 : #seat still available?
+     print("sell ticket")#tickets sold
+     seat = seat - 1 #no. of seats updated

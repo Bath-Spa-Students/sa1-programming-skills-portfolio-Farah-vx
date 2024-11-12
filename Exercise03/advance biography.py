7cf06901eba04723dfc6a39a9827292b8afc3d26
Farah-vx <ocyeanic.08@gmail.com>
2024-11-12 23:16:40 +0400

    advance biography.py

---
+++ Exercise03/advance biography.py
@@ -0,0 +1,7 @@
+#biography through advanced method
+name=input("enter your name: ")#user may input their first and second name
+hometown=input("enter your hometown: ")#user may input their homtown
+age=input("enter you age: ")#user may input their age
+
+#printing the info using sinle print statement
+print((f"name:{name}"),(f"hometown:{hometown}"),(f"age:{age}"))

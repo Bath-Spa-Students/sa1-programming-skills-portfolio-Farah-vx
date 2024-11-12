59fb926da1a5aa2fdc4000c68e48af4e725dedd7
Farah-vx <ocyeanic.08@gmail.com>
2024-11-12 23:19:38 +0400

    hello.py

---
+++ Exercise09/hello.py
@@ -0,0 +1,9 @@
+#fill in the blanks in the code below so the function hello() prints "hello"
+def hello():
+    print("hello") #fill in the blank to print "hello" to the console.
+
+def main():
+    hello() #fill in the blank to call the hello() function.
+
+if __name__ == "__main__":
+   main()

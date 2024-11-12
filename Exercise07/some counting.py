4b626a74168732a8eb8ba4170263eab0b582aead
Farah-vx <ocyeanic.08@gmail.com>
2024-11-12 23:19:03 +0400

    some counting.py

---
+++ Exercise07/some counting.py
@@ -0,0 +1,24 @@
+#composing loops that counts up from 0 to 50 in increments of 1
+print("counting up from 0 to 50 in increments of 1:")
+for i in range(0,51):
+    print(i)
+
+#" " " " "down from 50 to 0 in decrements of 1.
+print("\ncounting down from 50 to 0 in decrements of 1:")
+for num in range(50,-1,-1):
+    print(num)
+
+#" " " "up from 30 to 50 in increments of 1
+print("\ncounting up from 30 to 50 in increments of 1:")
+for x in range(30,51):
+    print(x)
+
+#" " " "down from 50 to 10 in decrements of 2
+print("\ncounting down from 50 to 10 in decrement of 2:")
+for y in range(50,9,-2):
+    print(y)
+
+#" " " "up from 100 to 200 in increments of 5
+print("\ncounting up from 100 to 200 in inrements of 5:")
+for z in range(100,201,5):
+    print(z)

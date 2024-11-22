7e8f9a373b3e8b9c417d7c92a037abf174c9cebe
Farah-vx <ocyeanic.08@gmail.com>
2024-11-22 22:36:17 +0400

    dictionaries.py

---
+++ z_bonus_exercise/dictionaries.py
@@ -0,0 +1,11 @@
+#storing information of a person in dictionary
+person = {"first_name" : "ramla",
+          "last_name" : "fariz",
+          "age" : "18",
+          "city" : "salmiya"}
+
+#printing each info
+print("first name:", person["first_name"])
+print("last name:", person["last_name"])
+print("age:", person["age"])
+print("city:", person["city"])

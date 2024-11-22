0cf45ee0b3b171deb00e836bb2807da79ca79fd8
Farah-vx <ocyeanic.08@gmail.com>
2024-11-22 22:36:04 +0400

    control stucture.py

---
+++ z_bonus_exercise/control stucture.py
@@ -0,0 +1,13 @@
+#assigning 'red' to the variable alien_color
+alien_color = "red"
+
+#using if statment 
+if alien_color == "red":
+    print("you have just earned 5 points")
+
+
+alien_color = "green"
+
+#checking if the color is red
+if alien_color == "red":
+    print("you have just earned 5 points")

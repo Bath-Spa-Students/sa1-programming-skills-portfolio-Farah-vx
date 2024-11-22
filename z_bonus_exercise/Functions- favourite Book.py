1b9a93237ba22251489994d0e34b7cec14485717
Farah-vx <ocyeanic.08@gmail.com>
2024-11-22 22:36:27 +0400

    Functions- favourite Book.py

---
+++ z_bonus_exercise/Functions- favourite Book.py
@@ -0,0 +1,7 @@
+#creating a function called favorite_book() that accepts the title
+def favorite_book(title):
+    return f"One of my favorite books is {title}."
+
+#calling the function
+text = favorite_book("Alice in Wonderland")
+print(text)

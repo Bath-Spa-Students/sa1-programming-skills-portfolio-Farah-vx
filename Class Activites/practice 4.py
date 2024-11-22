44ac2662c9461546110c661feadc63cd623bdced
Farah-vx <ocyeanic.08@gmail.com>
2024-11-22 23:42:09 +0400

    practice 4.py

---
+++ Class Activites/practice 4.py
@@ -0,0 +1,14 @@
+#Operator procedence BODMAS (Bracket,order division,multiplication,addition,substraction)
+
+print ((3*2)/2 +(5+5))
+
+#To write formula in multiple line we need back slash \
+result = 4 * 2 + 1 * 4 + \
+             4 * 6 + 5 * 2
+
+print (result)
+
+# end delimiter to remove new line , by default print will switch to another statement
+name = "Farah Rifad"
+print (name , end = " ")
+print (" New print line message ")

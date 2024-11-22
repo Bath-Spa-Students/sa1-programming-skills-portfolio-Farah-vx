626394f8dc4a1d3cbedcd0fbd1d1a86d92dbc1e5
Farah-vx <ocyeanic.08@gmail.com>
2024-11-22 23:41:19 +0400

    def greet ():     print ('hello')

---
+++ Class Activites/code lab practice 2.py
@@ -0,0 +1,29 @@
+x="bathspa"
+print(type(x))
+      
+y=10
+print(type(y))
+       
+Name="university"
+print(Name)
+
+#1String concatenation through variables
+message= "Hello" + "World"
+print(message)
+
+#2
+lesson= "creative " + "computing"
+print (lesson)
+
+#3
+college= "bathspa" + "university"
+print(college)
+
+#String concatenation through print statement 
+print("Hello "+ "World")
+
+print("Welcome to " +
+     "Bathspa university")
+
+print
+            

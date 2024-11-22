d17f328006d5b2e2a235bc34aa35b821f78eea93
Farah-vx <ocyeanic.08@gmail.com>
2024-11-22 23:41:59 +0400

    practice 3.py

---
+++ Class Activites/practice 3.py
@@ -0,0 +1,26 @@
+#To get input from user
+first_name = input (" Enter your name:")
+print (first_name)
+
+Last_name = input ("Enter your last name:")
+print (Last_name)
+
+#example 1
+Native_country = input (" Enter your native country:")
+print (Native_country)
+
+first_religion = input (" enter your native country:")
+print (first_religion)
+
+age = int(input("enter your age:"))
+print(Age)
+
+age = float(input("enter your age:"))
+
+#performing calculations
+print (8+ 2)
+print (7- 2)
+print (10* 4)
+print (30/2)   # For floatating point
+print (60//2)  # For integer
+

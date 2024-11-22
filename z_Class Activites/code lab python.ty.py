df4141950c5aac98bf21072b3d62d8edb08bedd6
Farah-vx <ocyeanic.08@gmail.com>
2024-11-22 23:46:50 +0400

    class activity

---
+++ z_Class Activites/code lab python.ty.py
@@ -0,0 +1,7 @@
+Na="Farah"
+Mn="Risna"
+Fn="Rifad"
+Sn="Afaf"
+Bn="Affan"
+B2="Luqman"
+print(Na,Mn,Fn,Sn,Bn,B2)
---
+++ z_Class Activites/practice 5.py
@@ -0,0 +1,10 @@
+#concanitaion
+string1 = "welcome"
+string2 = "to"
+string3 = " bathspa"
+
+
+result = string1 + " " + string2 + string3
+
+
+print(result)

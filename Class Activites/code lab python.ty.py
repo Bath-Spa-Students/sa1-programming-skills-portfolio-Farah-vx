a71b0581607bfa0e87f4703250c2493ed7f5054a
Farah-vx <ocyeanic.08@gmail.com>
2024-11-22 23:48:18 +0400

    class activity

--- Class Activites/code lab python.ty.py
+++
@@ -1,7 +0,0 @@
-Na="Farah"
-Mn="Risna"
-Fn="Rifad"
-Sn="Afaf"
-Bn="Affan"
-B2="Luqman"
-print(Na,Mn,Fn,Sn,Bn,B2)
--- Class Activites/practice 6.py
+++
@@ -1,8 +0,0 @@
-#decision structures
-sales = int(input("type sales:"))
-bonus = 0
-if sales > 6000:
-     bonus = 300
-else:
-     bonus = 0
-print ("your bonus is "+str(bonus))

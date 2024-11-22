9340e400d2b534e568be7c96e20b1f2aa22e3878
Farah-vx <ocyeanic.08@gmail.com>
2024-11-23 01:56:26 +0400

    advance biography.py

--- Exercise03/advance biography.py
+++ Exercise03/advance biography.py
@@ -3,5 +3,5 @@ name=input("enter your name: ")#user may input their first and second name
 hometown=input("enter your hometown: ")#user may input their homtown
 age=input("enter you age: ")#user may input their age
 
-#printing the info using sinle print statement
+#printing the info using single print statement
 print((f"name:{name}"),(f"hometown:{hometown}"),(f"age:{age}"))
